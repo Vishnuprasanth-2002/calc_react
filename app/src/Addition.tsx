f4668be1b68a1e5986ed9144c4590f8d5cebbfcf
Vishnuprasanth-2002 <s.vishnuprasanth14@gmail.com>
2023-10-03 23:29:52 +0530

    first commit

---
+++ app/src/Addition.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+function Addition(){
+    const [firstInput, setFirstInput] = useState<number>(0);
+    const [secondInput, setSecondInput] = useState<number>(0);
+    // function setValues{
+
+    // }
+    return <div className="container">
+        <div className="input-values">
+        <label htmlFor="first-value">First value</label>
+        <input type="number" name="first-value" id="first-value"/>
+        <label htmlFor="second-value">Second value</label>
+        <input type="number" name="{second-value}" id="second-value" />
+        </div>
+        <div className="buttons">
+        <button id="add">add</button>
+        <button id="sub">sub</button>
+        <button id="mul">mul</button>
+        <button id="divide">divide</button>
+        </div>
+        <div id="result"></div>
+    </div>
+
+
+}
+export default Addition;
---
+++ app/src/App.tsx
@@ -0,0 +1,12 @@
+import Addition from './Addition'
+import './App.css'
+
+function App() {
+  return (
+  
+    <Addition/>
+
+  )
+}
+
+export default App
